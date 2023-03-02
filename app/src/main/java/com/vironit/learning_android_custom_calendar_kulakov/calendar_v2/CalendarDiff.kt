0be52818f34b0aac724c8dad51e410bd861b1a5d
Alexander <alexkulakov01@gmail.com>
2023-03-02 10:08:20 +0300

    implement custom calendar, v2

---
+++ app/src/main/java/com/vironit/learning_android_custom_calendar_kulakov/calendar_v2/CalendarDiff.kt
@@ -0,0 +1,19 @@
+package com.vironit.learning_android_custom_calendar_kulakov.calendar_v2
+
+import androidx.recyclerview.widget.DiffUtil
+
+class CalendarDiff(private val old: List<Day>, private val new: List<Day>) : DiffUtil.Callback() {
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        old[oldItemPosition] == new[newItemPosition]
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        old[oldItemPosition].calendar.time == new[newItemPosition].calendar.time
+
+    override fun getOldListSize(): Int = old.size
+
+    override fun getNewListSize(): Int = new.size
+
+    fun calculateDiff(): DiffUtil.DiffResult {
+        return DiffUtil.calculateDiff(this, false)
+    }
+}
